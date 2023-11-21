6890c0ebdfcf361cb129696df228bb48b214052c
Emil Kristensen <emil00y1@stud.kea.dk>
2023-11-21 12:54:02 +0100

    things

--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export async function generateMetadata() {
   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
   const data = await res.json();
@@ -18,8 +20,16 @@ export default async function Henry() {
       <p>
         {name}&apos;s favourite color is {favouriteColor}
       </p>
-      {/*       <img src={data.image.url} alt="dog"></img>
-       */}
+      <Image
+        src={image.url}
+        alt={name}
+        width={image.width}
+        height={image.height}
+        priority={true} // disables lazy load
+        sizes="(max-width: 768px) 100vw,
+         (max-width: 1200px) 50vw,
+         400px"
+      />
     </main>
   );
 }
